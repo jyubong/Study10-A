4fda1a6cc1d54564da19bdbea7fbc25b5f70fd3b
jyubong <nane030@naver.com>
2023-09-09 11:02:52 +0900

    쥬봉이 코드 수정

--- Week2/jyubong/Jyubong/Camper.swift
+++ Week2/jyubong/Jyubong/Camper.swift
@@ -6,19 +6,30 @@
 //
 
 struct Camper {
+    var name: String
     var budget: Int = 2000
-    var shop = Shop()
+    var visitedShop: Shop
     
-    mutating func buy(productNumber: Int) throws {
-        // 예산확인
-        guard self.budget >= 1000 else {
-            throw ProductError.outOfMoney
+    func chooseProduct() throws -> Int {
+        visitedShop.displayProducts()
+        print("구매할 상품을 골라볼까? : ", terminator: "")
+        
+        guard let choice = readLine(), let choice = Int(choice) else {
+            throw ProductError.outOfNumber
         }
         
-        let product = try shop.manageProduct(productNumber: productNumber)
-        print("\(product)를 구매했습니다.")
-        
-        self.budget -= 1000
-        print("남은 예산이 \(budget)입니다.")
+        return choice
+    }
+    
+    mutating func buy(productNumber: Int) throws {
+            // 예산확인
+            guard self.budget >= 1000 else {
+                throw ProductError.outOfMoney
+            }
+            
+            let product = try visitedShop.manageProduct(productNumber: productNumber)
+            print("\(product)를 구매했습니다.")
+            self.budget -= 1000
+            print("남은 예산이 \(budget)입니다.\n")
     }
 }
