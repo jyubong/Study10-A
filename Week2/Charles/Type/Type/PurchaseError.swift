3ba060e093b42add2e0e082825b6d07b734c6f0f
charles <kch1759@naver.com>
2023-09-09 10:59:23 +0900

    Type

---
+++ Week2/Charles/Type/Type/PurchaseError.swift
@@ -0,0 +1,13 @@
+//
+//  PurchaseError.swift
+//  Type
+//
+//  Created by Charles on 2023/09/09.
+//
+
+import Foundation
+
+enum PurchaseError: Error {
+    case noneExistProduct
+    case overbudget
+}
