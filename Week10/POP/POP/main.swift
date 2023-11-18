4a5fc91ec773bc821d161fa06641b3c9affad54c
jyubong <nane030@naver.com>
2023-11-18 09:43:34 +0900

    feat: charger 인스턴스 생성

--- Week10/POP/POP/main.swift
+++ Week10/POP/POP/main.swift
@@ -5,6 +5,8 @@
 //  Created by Hisop on 2023/11/18.
 //
 
-import Foundation
-
-
+let appleWatchCharger = Charger(maximumWattPerHour: 5)
+let iPhoneCharger = Charger(maximumWattPerHour: 18)
+let iPadCharger = Charger(maximumWattPerHour: 30)
+let macBookCharger = Charger(maximumWattPerHour: 96)
+let macBookFasterCharger = Charger(maximumWattPerHour: 106)
